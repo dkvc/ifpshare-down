e1655cc731c5155c5519c67ef508e96e4befcd40
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-15 18:04:04 +0530

    [feat] Add constants used for utils

---
+++ src/types/constants.ts
@@ -0,0 +1,14 @@
+export const FETCH_API_PREFIX: string = 'https://workers-ifpsharedown.dhatri.dkvc.org/'
+
+export const FETCH_PUB_API_PREFIX: string = 'api/pub/files/'
+
+export const PREFIXES: string[] = [
+  'https://air.ifpshare.com/',
+  'https://www.air.ifpshare.com/',
+  'http://air.ifpshare.com/',
+  'http://www.air.ifpshare.com/',
+  '//air.ifpshare.com/',
+  '//www.air.ifpshare.com/',
+  'air.ifpshare.com/',
+  'www.air.ifpshare.com/',
+]
