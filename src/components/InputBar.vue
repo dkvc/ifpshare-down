e8c5c55c4a49cb91b353a856d1989859c9beface
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-15 10:36:48 +0530

    [feat] Add Input Bar component

---
+++ src/components/InputBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+/* This component is not meant to be reusable for other projects */
+import { ref } from 'vue'
+import InputGroup from 'primevue/inputgroup'
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import ProgressSpinner from 'primevue/progressspinner'
+import Toast from 'primevue/toast'
+import { useToast } from 'primevue'
+
+</script>
+
+<template>
+  <InputGroup>
+    <InputText placeholder="https://ifpshare.com/..." />
+    <Button label="Create PDF" @click="createPDF" />
+  </InputGroup>
+
+  <Toast position="bottom-right" />
+</template>
