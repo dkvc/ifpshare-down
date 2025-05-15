efb85745c545ce2fb59f3dfc3e33cd9fdbeabad1
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-15 20:22:23 +0530

    [feat] Add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import FooterBar from '@/components/FooterBar.vue'
 import HomeView from '@/views/HomeView.vue'
 </script>
 
 <template>
   <HomeView />
+  <FooterBar />
 </template>
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+import Panel from 'primevue/panel'
+</script>
+
+<template>
+  <footer>
+    <Panel>
+      <div class="footer-bar">
+        <p>© MIT License</p>
+        <p id="appVersion">v0.1.0</p>
+        <div class="footer-bar-icons">
+          <Button
+            as="a"
+            label="GitHub"
+            href="https://github.com/dkvc/ifpshare-down/"
+            icon="pi pi-github"
+            target="_blank"
+            rel="noopener"
+            variant="outlined"
+            size="small"
+          />
+        </div>
+      </div>
+    </Panel>
+  </footer>
+</template>
+
+<style scoped>
+.footer-bar {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  color: var(--p-text-muted-color);
+}
+
+.footer-bar-icons {
+  display: flex;
+}
+
+#appVersion {
+  font-family: 'IBM Plex Sans';
+  font-weight: 500;
+}
+</style>
