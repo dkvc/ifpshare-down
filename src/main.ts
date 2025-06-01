bb13128b319210a938e2de53628b8f6e6b430036
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-06-01 14:26:54 +0530

    [fix] Improve color contrast for light mode

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,24 @@ import FocusTrap from 'primevue/focustrap'
 import Lara from '@primeuix/themes/lara'
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
+import { definePreset } from '@primeuix/themes'
 
 const app = createApp(App)
+const themePreset = definePreset(Lara, {
+  semantic: {
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{emerald:950}',
+        },
+      },
+    },
+  },
+})
+
 app.use(PrimeVue, {
   theme: {
-    preset: Lara,
+    preset: themePreset,
     options: {
       darkModeSelector: 'system',
     },
